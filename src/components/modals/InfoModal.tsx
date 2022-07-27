78e6547913a73b017f5557f5a18d5987fd84d90b
hamSanta <snhavanki@gmail.com>
2022-07-26 20:53:17 -0400

    changed environmental vars and updated info

--- src/components/modals/InfoModal.tsx
+++ src/components/modals/InfoModal.tsx
@@ -11,62 +11,75 @@ export const InfoModal = ({ isOpen, handleClose }: Props) => {
     <BaseModal title="How to play" isOpen={isOpen} handleClose={handleClose}>
       <p className="text-sm text-gray-500 dark:text-gray-300">
         Guess the word in 6 tries. After each guess, the color of the tiles will
-        change to show how close your guess was to the word.
+        change to show how close your guess was to the word. 
       </p>
 
       <div className="flex justify-center mb-1 mt-4">
-        <Cell
-          isRevealing={true}
-          isCompleted={true}
-          value="W"
+        <Cell value="C" />
+        <Cell 
+          isRevealing={true} 
+          isCompleted={true} 
+          value="R" 
           status="correct"
         />
-        <Cell value="E" />
-        <Cell value="A" />
-        <Cell value="R" />
-        <Cell value="Y" />
+        <Cell value="O" />
+        <Cell value="W" />
+        <Cell value="N" />
       </div>
       <p className="text-sm text-gray-500 dark:text-gray-300">
-        The letter W is in the word and in the correct spot.
+        The letter R is in the word and in the correct spot.
       </p>
 
       <div className="flex justify-center mb-1 mt-4">
-        <Cell value="P" />
-        <Cell value="I" />
-        <Cell
+        <Cell value="G" />
+        <Cell value="A" />
+        <Cell value="C" />
+        <Cell 
           isRevealing={true}
           isCompleted={true}
-          value="L"
+          value="H" 
           status="present"
         />
-        <Cell value="O" />
-        <Cell value="T" />
+        <Cell value="A" />
       </div>
       <p className="text-sm text-gray-500 dark:text-gray-300">
-        The letter L is in the word but in the wrong spot.
+        The letter H is in the word but in the wrong spot.
       </p>
 
       <div className="flex justify-center mb-1 mt-4">
-        <Cell value="V" />
-        <Cell value="A" />
-        <Cell value="G" />
-        <Cell isRevealing={true} isCompleted={true} value="U" status="absent" />
-        <Cell value="E" />
+        <Cell isRevealing={true} isCompleted={true} value="A" status="absent" />
+        <Cell value="B" />
+        <Cell value="Y" />
+        <Cell value="S" />
+        <Cell value="S" />
       </div>
       <p className="text-sm text-gray-500 dark:text-gray-300">
-        The letter U is not in the word in any spot.
+        The letter A is not in the word in any spot.
       </p>
 
       <p className="mt-6 italic text-sm text-gray-500 dark:text-gray-300">
-        This is an open source version of the word guessing game we all know and
+        Based on an open source version of the word guessing game we all know and
         love -{' '}
         <a
           href="https://github.com/cwackerfuss/react-wordle"
           className="underline font-bold"
         >
-          check out the code here
+          check out the code here!
         </a>{' '}
       </p>
+
+      <p className="mt-6 italic text-sm text-gray-500 dark:text-gray-300">
+        Inspired by {' '}
+        <a href="https://paimordle.vercel.app/" className="underline font-bold">
+          Paimordle 
+        </a>{' '}
+        and {' '}
+        <a href="https://paimordle-vi.vercel.app/" className="underline font-bold">
+          Paimordle IV
+        </a>{' '}
+        - go play them!
+      </p>
+
     </BaseModal>
   )
 }
